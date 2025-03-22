40f9a2a9f61750a9725dfdfe471c178423fb3458
Yakup Budanaz <budanaz.yakup@gmail.com>
2025-03-22 14:18:53 +0100

    Improve flags

--- velocity/utils/compile_sdfg.py
+++ velocity/utils/compile_sdfg.py
@@ -72,12 +72,12 @@ def compile_sdfg(sdfg: dace.SDFG, gpu: bool = False, release: bool = False):
         if release:
             flags = " -std=c++20 -Xcompiler=-Wall -Xcompiler=-Wextra -Xcompiler=-Wno-unused-parameter -Xcompiler=-Wno-unknown-pargmas -Xcompiler=-g -Xcompiler=-O3 -lineinfo -Xcompiler=-faligned-new -Xcompiler=-fopenmp --expt-relaxed-constexpr -arch=native"
         else:
-            flags = " -std=c++20 -Xcompiler=-Wall -Xcompiler=-Wextra -Xcompiler=-Wno-unused-parameter -Xcompiler=-Wno-unknown-pargmas -Xcompiler=-faligned-new --expt-relaxed-constexpr -arch=native -O0 -Xcompiler=-O0 -lineinfo -G -g -Xcompiler=-g"
+            flags = " -std=c++20 -Xcompiler=-Wall -Xcompiler=-Wextra -Xcompiler=-Wno-unused-parameter -Xcompiler=-Wno-unknown-pargmas -Xcompiler=-faligned-new --expt-relaxed-constexpr -arch=native -O0 -Xcompiler=-O0 -lineinfo -G -g -Xcompiler=-g --fmad=false --prec-div=true --prec-sqrt=true --ftz=false --fprecision=high --use_fast_math=false"
     else:
         if release:
-            flags = " -std=c++20 -Wall -Wextra -Wno-unused-parameter -fopenmp -faligned-new -O3 -g "
+            flags = " -std=c++20 -Wall -Wextra -Wno-unused-parameter -Wno-unused-variable -fopenmp -faligned-new -O3 -g "
         else:
-            flags = " -std=c++20 -Wall -Wextra -Wno-unused-parameter -Wno-unknown-pargmas -faligned-new -O0 -g -ggdb "
+            flags = " -std=c++20 -Wall -Wextra -Wno-unused-parameter -Wno-unused-variable -Wno-unknown-pragmas -faligned-new -O0 -g -ggdb "
 
     if gpu:
         exit_code = os.system(
