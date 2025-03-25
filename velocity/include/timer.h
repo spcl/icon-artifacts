acfa094e9c4cfcb59fbd391e21d4a485f200cb0b
Berke Ates <berke@ates.ch>
2025-03-25 10:31:02 +0100

    added timer check function

--- velocity/include/timer.h
+++ velocity/include/timer.h
@@ -17,3 +17,16 @@ void measure_time(velocity_tendencies_state_t *__state = nullptr, const char *ta
   timer_last_time = std::chrono::high_resolution_clock::now();
   timer_call_count++;
 }
+
+void timer_1s_check(velocity_tendencies_state_t *__state = nullptr)
+{
+  auto begin = std::chrono::high_resolution_clock::now();
+
+  // Sleep for 1 second
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+
+  auto now = std::chrono::high_resolution_clock::now();
+  unsigned long int start = std::chrono::duration_cast<std::chrono::microseconds>(begin.time_since_epoch()).count();
+  unsigned long int end = std::chrono::duration_cast<std::chrono::microseconds>(now.time_since_epoch()).count();
+  __state->report.add_completion("timer 1s check", "Timer", start, end, 0, 0, 0);
+}
