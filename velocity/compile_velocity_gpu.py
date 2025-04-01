18ec494be80726ba98d0bcc1c5869f0199bd315e
Yakup Budanaz <budanaz.yakup@gmail.com>
2025-04-02 00:48:51 +0200

    Various fixes (hacks) to compile gpu

--- velocity/compile_velocity_gpu.py
+++ velocity/compile_velocity_gpu.py
@@ -169,11 +169,7 @@ for sdfg_name in sdfg_names:
                         n.map.schedule = dace.ScheduleType.Sequential
 
         prune_unused_inputs_outputs(sdfg)
-        sdfg.apply_transformations_repeated(StateFusion)
-        for n, g in sdfg.all_nodes_recursive():
-            if isinstance(n, dace.nodes.NestedSDFG):
-                if isinstance(n, dace.nodes.NestedSDFG):
-                    n.sdfg.apply_transformations_repeated(StateFusion, permissive=True)
+
         InlineSDFGs().apply_pass(sdfg, {})
         k = sdfg.apply_transformations_repeated(MapCollapse, permissive=True)
         if verbose:
@@ -201,13 +197,7 @@ for sdfg_name in sdfg_names:
         # I saw trurthy ifs, propagate those conditions and try to fuse states agian
         propagate_if_cond(sdfg, sdfg, None, None, verbose)
         demote_symbol_to_scalar(sdfg, "tmp_call_18")
-        sdfg.apply_transformations_repeated(StateFusion)
-        for n, g in sdfg.all_nodes_recursive():
-            if isinstance(n, dace.nodes.NestedSDFG):
-                if isinstance(n, dace.nodes.NestedSDFG):
-                    n.sdfg.apply_transformations_repeated(StateFusion, permissive=True)
-        propagate_if_cond(sdfg, sdfg, None, None, verbose)
-        sdfg.apply_transformations_repeated(StateFusion)
+
         sdfg.validate()
 
         # Breaks the SDFG
@@ -225,9 +215,22 @@ for sdfg_name in sdfg_names:
     if Path(f"gpu_{sdfg_name}_stage5.sdfgz").exists() and use_cache:
         sdfg = dace.SDFG.from_file(f"gpu_{sdfg_name}_stage5.sdfgz")
     else:
+        sdfg.validate()
+        prune_unused_inputs_outputs(sdfg)
+        sdfg.validate()
 
-        ToGPU().apply_pass(sdfg, {"verbose": verbose})
+        ToGPU(verbose=verbose).apply_pass(sdfg, {})
 
+        sdfg.validate()
+        prune_unused_inputs_outputs(sdfg)
+        sdfg.validate()
+        sdfg.apply_transformations_repeated(StateFusion)
+        for n, g in sdfg.all_nodes_recursive():
+            if isinstance(n, dace.nodes.NestedSDFG):
+                if isinstance(n, dace.nodes.NestedSDFG):
+                    n.sdfg.apply_transformations_repeated(StateFusion, permissive=True)
+        propagate_if_cond(sdfg, sdfg, None, None, verbose)
+        sdfg.apply_transformations_repeated(StateFusion)
         #if not reduction:
         #    for cfg in sdfg.nodes():
         #        if cfg.label == "FOR_l_568_c_568{sdfg.function_suffix}":
@@ -237,7 +240,7 @@ for sdfg_name in sdfg_names:
         #            s.add_edge(a0, None, a1, None, dace.Memlet(expr="gpu_vcflmax"))
 
         GPUKernelLaunchRestructure().apply_pass(sdfg, {})
-        prune_unused_inputs_outputs(sdfg)
+        sdfg.validate()
 
         sdfg.validate()
         if use_cache:
