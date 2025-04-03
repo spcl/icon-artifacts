c2199010a55b0abbb181b6d67dcdcb27cdc51997
Pratyai Mazumder <pratyai.mazumder@gmail.com>
2025-04-04 00:29:54 +0200

    reformat + manual edits`

---
+++ cloudsc/restyle.sh
@@ -0,0 +1,4 @@
+clang-format -i struct_defs.h || exit 1
+clang-format -i cloudsc-simplified-codegen/src/cpu/cloudsc_driver.cpp || exit 1
+clang-format -i cloudsc-simplified-loop2map-codegen/src/cpu/cloudsc_driver.cpp || exit 1
+
