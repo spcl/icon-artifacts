d1d8e502697ca19e0918db7d12a2a3f629489685
Yakup Budanaz <budanaz.yakup@gmail.com>
2025-04-06 23:22:07 +0200

    Fix dev reduce

--- velocity/include/reductions_device.cuh
+++ velocity/include/reductions_device.cuh
@@ -11,7 +11,7 @@ __device__ __inline__ double reduce_maxZ_to_scalar_device(const double *d_in, in
     return max_val;
 }
 
-__device__ __inline__ void reduce_maxZ_to_scalar_device(const double *d_in, double* d_out, int size)
+__device__ __inline__ void reduce_maxZ_to_address_device(const double *d_in, double* d_out, int size)
 {
     d_out[0] = reduce_maxZ_to_scalar_device(d_in, size);
 }
@@ -26,7 +26,7 @@ __device__ __inline__ int reduce_sum_to_scalar_device(const int *d_in, int size)
     return sum;
 }
 
-__device__ __inline__ void reduce_maxZ_to_address_device(const int *d_in, int* d_out, int size)
+__device__ __inline__ void reduce_sum_to_address_device(const int *d_in, int* d_out, int size)
 {
     d_out[0] = reduce_sum_to_scalar_device(d_in, size);
 }
@@ -37,19 +37,3 @@ __device__ __inline__ int reduce_scan_device(const int *d_in, int size)
     return (reduce_sum_to_scalar_device(d_in, size) > 0) ? 1 : 0;
 }
 
-__device__ __inline__ double reduce_maxZ_device(const double d_in, int size)
-{
-    return d_in;
-}
-
-// sum reduction interface
-__device__ __inline__ int reduce_sum_device(const int d_in, int size)
-{
-    return d_in;
-}
-
-// scan reduction interface
-__device__ __inline__ int reduce_scan_device(const int d_in, int size)
-{
-    return (d_in > 0) ? 1 : 0;
-}
