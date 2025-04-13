ef726ba94bcdcb736a031d9f55d8c4eca80c3b59
Yakup Budanaz <budanaz.yakup@gmail.com>
2025-04-13 16:46:57 +0200

    sync before running

--- velocity/utils/compile_if_propagated_sdfgs.py
+++ velocity/utils/compile_if_propagated_sdfgs.py
@@ -132,13 +132,13 @@ def add_timers(file_path: str):
 
     # Pattern 1: Insert BEFORE `nrdmax_jg = __CG_global_data__m_nrdmax[0];`
     pattern1 = r'^\s*nrdmax_jg\s*=\s*__CG_global_data__m_nrdmax\[0\];\s*$'
-    replacement1 = '    measure_time("Run");\n\\g<0>'
+    replacement1 = '    cudaDeviceSynchronize();\n    measure_time("Run");\n\\g<0>'
 
     # Pattern 2: Insert AFTER `__CG_p_diag__m_max_vcfl_dyn = p_diag_out_max_vcfl_dyn;`
     pattern2 = r'^(.*i_endblk_var_147 = __CG_p_patch__CG_cells__m_end_block\[\(\(- __f2dace_SOA_end_block_d_0_s_163_cells_p_patch_2\) - 4\)\];.*)$'
     p2 = "i_endblk_var_147 = __CG_p_patch__CG_cells__m_end_block[((- __f2dace_SOA_end_block_d_0_s_163_cells_p_patch_2) - 4)];"
     escaped_p2 = re.escape(p2)
-    replacement2 = '\\1\n    measure_time("Run");'
+    replacement2 = '\\1\n    cudaDeviceSynchronize();\n    measure_time("Run");'
 
     # Apply replacements
     code = re.sub(pattern1, replacement1, code, flags=re.MULTILINE)
