5b5d6607f119ab84de0c0b7e95644c2793fd0ec5
Pratyai Mazumder <pmazumde@ault.cscs.ch>
2025-05-05 18:39:24 +0200

    Add the script upto aopt.

--- cloudsc/cloudsc.py
+++ cloudsc/cloudsc.py
@@ -1,5 +1,24 @@
-from dace import SDFG
+from dace import SDFG, DeviceType
+from dace.transformation.auto import auto_optimize as aopt
 from dace.transformation.interstate import LoopToMap
+
+'''
+g = SDFG.from_file("cloudsc.sdfg")
+g.build_folder = "./cloudsc-codegen"
+g.compile()
+g.simplify()
+g.save('cloudsc-simplified.sdfg')
+g.build_folder = './cloudsc-simplified-codegen'
+g.compile()
+exit()
+'''
+
+g = SDFG.from_file("cloudsc-simplified.sdfg")
+aopt.auto_optimize(g, DeviceType.CPU)
+g.save('cloudsc-simplified-aopt.sdfg')
+g.build_folder = './cloudsc-simplified-aopt-codegen'
+g.compile()
+exit()
 
 '''
 g = SDFG.from_file("cloudsc-simplified.sdfgz")
