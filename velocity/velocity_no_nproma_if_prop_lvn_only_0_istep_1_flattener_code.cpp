659e47dab9d864eb739202f85361c3438305eb9e
Yakup Budanaz <budanaz.yakup@gmail.com>
2025-06-24 21:41:37 +0200

    Add flattener codes

---
+++ velocity/velocity_no_nproma_if_prop_lvn_only_0_istep_1_flattener_code.cpp
@@ -0,0 +1,130 @@
+
+__CG_global_data__m_nflatlev = global_data->nflatlev;
+
+__CG_global_data__m_nproma = global_data->nproma;
+
+__CG_global_data__m_nrdmax = global_data->nrdmax;
+
+
+gpu___CG_p_patch__CG_cells__CG_decomp_info__m_owner_mask = p_patch->cells->decomp_info->owner_mask;
+
+gpu___CG_p_patch__CG_cells__m_neighbor_idx = p_patch->cells->neighbor_idx;
+
+gpu___CG_p_patch__CG_cells__m_neighbor_blk = p_patch->cells->neighbor_blk;
+
+gpu___CG_p_patch__CG_cells__m_edge_idx = p_patch->cells->edge_idx;
+
+gpu___CG_p_patch__CG_cells__m_edge_blk = p_patch->cells->edge_blk;
+
+gpu___CG_p_patch__CG_cells__m_area = p_patch->cells->area;
+
+__CG_p_patch__CG_cells__m_start_index = p_patch->cells->start_index;
+
+__CG_p_patch__CG_cells__m_end_index = p_patch->cells->end_index;
+
+__CG_p_patch__CG_cells__m_start_block = p_patch->cells->start_block;
+
+__CG_p_patch__CG_cells__m_end_block = p_patch->cells->end_block;
+
+gpu___CG_p_patch__CG_edges__m_cell_idx = p_patch->edges->cell_idx;
+
+gpu___CG_p_patch__CG_edges__m_cell_blk = p_patch->edges->cell_blk;
+
+gpu___CG_p_patch__CG_edges__m_vertex_idx = p_patch->edges->vertex_idx;
+
+gpu___CG_p_patch__CG_edges__m_vertex_blk = p_patch->edges->vertex_blk;
+
+gpu___CG_p_patch__CG_edges__m_tangent_orientation = p_patch->edges->tangent_orientation;
+
+gpu___CG_p_patch__CG_edges__m_quad_idx = p_patch->edges->quad_idx;
+
+gpu___CG_p_patch__CG_edges__m_quad_blk = p_patch->edges->quad_blk;
+
+gpu___CG_p_patch__CG_edges__m_inv_primal_edge_length = p_patch->edges->inv_primal_edge_length;
+
+gpu___CG_p_patch__CG_edges__m_inv_dual_edge_length = p_patch->edges->inv_dual_edge_length;
+
+gpu___CG_p_patch__CG_edges__m_area_edge = p_patch->edges->area_edge;
+
+gpu___CG_p_patch__CG_edges__m_f_e = p_patch->edges->f_e;
+
+__CG_p_patch__CG_edges__m_start_index = p_patch->edges->start_index;
+
+__CG_p_patch__CG_edges__m_end_index = p_patch->edges->end_index;
+
+__CG_p_patch__CG_edges__m_start_block = p_patch->edges->start_block;
+
+__CG_p_patch__CG_edges__m_end_block = p_patch->edges->end_block;
+
+gpu___CG_p_patch__CG_verts__m_cell_idx = p_patch->verts->cell_idx;
+
+gpu___CG_p_patch__CG_verts__m_cell_blk = p_patch->verts->cell_blk;
+
+gpu___CG_p_patch__CG_verts__m_edge_idx = p_patch->verts->edge_idx;
+
+gpu___CG_p_patch__CG_verts__m_edge_blk = p_patch->verts->edge_blk;
+
+__CG_p_patch__CG_verts__m_start_index = p_patch->verts->start_index;
+
+__CG_p_patch__CG_verts__m_end_index = p_patch->verts->end_index;
+
+__CG_p_patch__CG_verts__m_start_block = p_patch->verts->start_block;
+
+__CG_p_patch__CG_verts__m_end_block = p_patch->verts->end_block;
+
+
+gpu___CG_p_int__m_c_lin_e = p_int->c_lin_e;
+
+gpu___CG_p_int__m_e_bln_c_s = p_int->e_bln_c_s;
+
+gpu___CG_p_int__m_cells_aw_verts = p_int->cells_aw_verts;
+
+gpu___CG_p_int__m_rbf_vec_coeff_e = p_int->rbf_vec_coeff_e;
+
+gpu___CG_p_int__m_geofac_grdiv = p_int->geofac_grdiv;
+
+gpu___CG_p_int__m_geofac_rot = p_int->geofac_rot;
+
+gpu___CG_p_int__m_geofac_n2s = p_int->geofac_n2s;
+
+
+gpu___CG_p_prog__m_w = p_prog->w;
+
+gpu___CG_p_prog__m_vn = p_prog->vn;
+
+
+gpu___CG_p_metrics__m_ddxn_z_full = p_metrics->ddxn_z_full;
+
+gpu___CG_p_metrics__m_ddxt_z_full = p_metrics->ddxt_z_full;
+
+gpu___CG_p_metrics__m_ddqz_z_full_e = p_metrics->ddqz_z_full_e;
+
+gpu___CG_p_metrics__m_ddqz_z_half = p_metrics->ddqz_z_half;
+
+gpu___CG_p_metrics__m_wgtfac_c = p_metrics->wgtfac_c;
+
+gpu___CG_p_metrics__m_wgtfac_e = p_metrics->wgtfac_e;
+
+gpu___CG_p_metrics__m_wgtfacq_e = p_metrics->wgtfacq_e;
+
+gpu___CG_p_metrics__m_coeff_gradekin = p_metrics->coeff_gradekin;
+
+gpu___CG_p_metrics__m_coeff1_dwdz = p_metrics->coeff1_dwdz;
+
+gpu___CG_p_metrics__m_coeff2_dwdz = p_metrics->coeff2_dwdz;
+
+
+__CG_p_diag__m_max_vcfl_dyn = p_diag->max_vcfl_dyn;
+
+gpu___CG_p_diag__m_vt = p_diag->vt;
+
+gpu___CG_p_diag__m_vn_ie = p_diag->vn_ie;
+
+gpu___CG_p_diag__m_w_concorr_c = p_diag->w_concorr_c;
+
+gpu___CG_p_diag__m_ddt_vn_apc_pc = p_diag->ddt_vn_apc_pc;
+
+gpu___CG_p_diag__m_ddt_w_adv_pc = p_diag->ddt_w_adv_pc;
+
+
+
