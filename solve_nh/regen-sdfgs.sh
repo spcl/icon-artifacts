78659d3c58ca5d1c93b1accbe45650366efe5b71
Pratyai Mazumder <pmz@pmzx.local>
2025-07-10 09:50:12 +0200

    Update script; no more manual change.

--- solve_nh/regen-sdfgs.sh
+++ solve_nh/regen-sdfgs.sh
@@ -19,6 +19,19 @@ python -m dace.frontend.fortran.tools.create_preprocessed_ast \
   -k fake_mo_solve_nonhydro.solve_nh_corrector_pre \
   -k fake_mo_solve_nonhydro.solve_nh_corrector_post
 
+cp solve_nh_fake.f90 solve_nh_fake.f90.bak \
+  && sed -E -e 's/[[:space:]]*=>[[:space:]]*null\([[:space:]]*\)//Ig' solve_nh_fake.f90.bak > solve_nh_fake.f90
+cp solve_nh_fake.f90 solve_nh_fake.f90.bak \
+  && sed -E -e 's/\(process_mpi_all_size <= 1\)/.TRUE./Ig' solve_nh_fake.f90.bak > solve_nh_fake.f90
+cp solve_nh_fake.f90 solve_nh_fake.f90.bak \
+  && awk '
+    BEGIN {skip=0}
+    /IF *\(generation == 20\) *THEN/ {skip=1; next}
+    skip && /END *IF/ {skip=0; next}
+    !skip {print}
+  ' solve_nh_fake.f90.bak > solve_nh_fake.f90
+rm solve_nh_fake.f90.bak
+
 python -m utils.prune_unused_args \
   -i solve_nh_fake.f90 \
   -o solve_nh_fake.f90 \
@@ -26,10 +39,6 @@ python -m utils.prune_unused_args \
   -k fake_mo_solve_nonhydro.solve_nh_predictor_post \
   -k fake_mo_solve_nonhydro.solve_nh_corrector_pre \
   -k fake_mo_solve_nonhydro.solve_nh_corrector_post
-
-cp solve_nh_fake.f90 solve_nh_fake.f90.bak \
-  && sed -E -e 's/[[:space:]]*=>[[:space:]]*null\([[:space:]]*\)//Ig' solve_nh_fake.f90.bak > solve_nh_fake.f90
-rm solve_nh_fake.f90.bak
 
 python -m dace.frontend.fortran.tools.create_singular_sdfg_from_ast \
   -i solve_nh_fake.f90 \
