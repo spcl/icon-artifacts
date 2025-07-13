d4617889ef6f866c27b0e75b45a4d7354d42af3b
Yakup Budanaz <budanaz.yakup@gmail.com>
2025-07-13 16:19:12 +0200

    new build script

--- velocity/build.sh
+++ velocity/build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-for STAGE in {1..9}; do
-    python -m utils.stages.compile_gpu_stage${STAGE} --optimize --compile 1>>build.out 2>>build.err
+RELEASE_MODES=(FALSE TRUE)
+
+for _RELEASE in "${RELEASE_MODES[@]}"; do
+    export RELEASE=${_RELEASE}
+    export _USE_CUDA_EVENTS=0
+    export _RELEASE=${_RELEASE}
+    export _TILE=0
+    export _REDUCE_BITWIDTH_TRANSFORMATION=1
+    export _STANDALONE_HEADERS=1
+    for STAGE in {1..9}; do
+        python -m utils.stages.compile_gpu_stage${STAGE} --optimize --compile
+    done
 done
