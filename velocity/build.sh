0e5b316e0b35c1773951f538d4060423d0eac00e
Yakup Budanaz <budanaz.yakup@gmail.com>
2025-07-13 16:54:01 +0200

    Fix build script

--- velocity/build.sh
+++ velocity/build.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 RELEASE_MODES=(FALSE TRUE)
+INTEGRATION_MODES=(0 1)
 
 for _R in "${RELEASE_MODES[@]}"; do
-    for _BL in {0,1}; do
+    for _BL in "${INTEGRATION_MODES[@]}"; do
         export _USE_CUDA_EVENTS=0
         export _RELEASE=${_R}
-        export RELEASE=${_R}
         export _TILE=0
         export _REDUCE_BITWIDTH_TRANSFORMATION=1
-        export _STANDALONE_HEADERS=1
         export _BUILD_LIB_FOR_SOLVE_NH=${BL}
         for STAGE in {1..9}; do
             python -m utils.stages.compile_gpu_stage${STAGE} --optimize --compile
