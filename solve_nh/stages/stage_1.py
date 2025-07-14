408ac1f25975efa19f643097f6810c4de6317b77
Berke Ates <berke@ates.ch>
2025-07-14 15:21:27 +0200

    added stage 1

---
+++ solve_nh/stages/stage_1.py
@@ -0,0 +1,101 @@
+import dace
+from dace import SDFG
+from utils.inject_velocity_shim import inject_velocity_shim
+from stages import common
+from utils.codegen_from_sdfg import Mode
+
+import argparse
+
+from dace.transformation.interstate import ContinueToCondition, LoopToMap
+from dace.transformation.passes import (
+    StructToContainerGroups,
+    SymbolPropagation,
+    ConstantPropagation,
+)
+
+from utils.count import count_loops
+
+STAGE_ID = 1
+
+
+def optimization_action(g: SDFG):
+    """DEFINE THE OPTIMIZATION ACTION HERE"""
+    g.apply_transformations_repeated(ContinueToCondition)
+    StructToContainerGroups(
+        validate=False,
+        save_steps=False,
+        verbose=False,
+        simplify=False,
+        interface_with_struct_copy=True,
+        interface_to_gpu=False,
+        clean_trivial_views=True,
+        shallow_copy=False,
+        shallow_copy_to_gpu=False,
+        taskloop=False,
+    ).apply_pass(g, {})
+    g.simplify(skip=["ArrayElimination"])
+    SymbolPropagation().apply_pass(g, {})
+    g.simplify(skip=["ArrayElimination"])
+    ConstantPropagation().apply_pass(g, {})
+
+    g.apply_transformations_repeated(
+        LoopToMap, permissive=True, options={"ballin": True}
+    )
+    count_loops(g, verbose=False, use_assert=True)
+    return g
+
+
+def main():
+    argp = argparse.ArgumentParser()
+    argp.add_argument(
+        "--optimize", action=argparse.BooleanOptionalAction, default=False
+    )
+    argp.add_argument("--codegen", action=argparse.BooleanOptionalAction, default=False)
+    argp.add_argument("--compile", action=argparse.BooleanOptionalAction, default=False)
+    argp.add_argument(
+        "--mode",
+        type=Mode,
+        choices=list(Mode),
+        required=False,
+        default=Mode.EXEC,
+        help="Select the mode: static, shared, or exec",
+    )
+    args = argp.parse_args()
+    if not args.optimize and not args.codegen and not args.compile:
+        args.optimize, args.codegen, args.compile = True, True, True
+
+    names = common.sdfg_names()
+
+    if args.optimize:
+        sdfgs = {}
+        for name in names:
+            infile = common.stage_input(name, STAGE_ID)
+            outfile = common.stage_output(name, STAGE_ID)
+
+            print(f"Stage #{STAGE_ID}: Optimising {name} from {infile}")
+
+            g = SDFG.from_file(infile)
+            g.name = name
+            g.validate()
+
+            g = optimization_action(g)
+
+            g.save(outfile, compress=True)
+            print(f"Stage #{STAGE_ID}: Saved as {outfile}")
+            sdfgs[name] = g
+
+    if args.codegen:
+        # Read back the written files as we prepare for compilation.
+        sdfgs = {
+            name: SDFG.from_file(common.stage_output(name, STAGE_ID)) for name in names
+        }
+        print(f"Stage #{STAGE_ID}: Generating code for {len(sdfgs)} SDFGs")
+        common.codegen_action(STAGE_ID, sdfgs)
+
+    if args.compile:
+        print(f"Stage #{STAGE_ID}: Compiling SDFGs")
+        common.compile_action(STAGE_ID, args.mode)
+
+
+if __name__ == "__main__":
+    main()
