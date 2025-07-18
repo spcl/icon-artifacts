77ff6946b1e950eafcb9992f5331cec2973300d7
Pratyai Mazumder <pmazumde@ault.cscs.ch>
2025-07-18 16:05:22 +0200

    Pull from the new set of libraries.

--- solve_nh/regen-velocity-lib.sh
+++ solve_nh/regen-velocity-lib.sh
@@ -4,16 +4,15 @@ set -xeuo pipefail
 
 # --- Configuration ---
 # Source git branch for the precompiled libraries
-GIT_SOURCE_BRANCH="origin/libs"
+GIT_SOURCE_BRANCH="origin/new_sched_solve_nh"
 # Base path for the precompiled libraries within the git repository
-VELOCITY_SRC_BASE_PATH="libraries"
+VELOCITY_SHARED_LIB_ROOT="velocity/shared_libs"
+MACHINE="ault09.cscs.ch"
+LIB_MODE="solve_nh_integration"
+BUILD_MODE="debug" # Options: debug, release
 
-VT_STAGE_1_AULT_SO="libvelocity_gpu_stage1_x86_a100_cpu_input_solve_nh_integration_release.so"
-
-for N in 0 1 2; do
-  SO_NAME="libvelocity_gpu_stage1_x86_a100_cpu_input_solve_nh_integration_release.so"
-  OUTPUT_SO="libvelocity_${N}.so"
-  echo "Fetching $SO_NAME from git for $OUTPUT_SO..."
-  git show "$GIT_SOURCE_BRANCH:$VELOCITY_SRC_BASE_PATH/$SO_NAME" > "$OUTPUT_SO"
-  echo "Saved as $OUTPUT_SO"
-done
+# For stages 0-3, we will fetch the same VT shared library file built at stage 1.
+git show "${GIT_SOURCE_BRANCH}:${VELOCITY_SHARED_LIB_ROOT}/${MACHINE}/${LIB_MODE}/${BUILD_MODE}/1/libvelocity.so" > "libvelocity_0.so"
+git show "${GIT_SOURCE_BRANCH}:${VELOCITY_SHARED_LIB_ROOT}/${MACHINE}/${LIB_MODE}/${BUILD_MODE}/1/libvelocity.so" > "libvelocity_1.so"
+git show "${GIT_SOURCE_BRANCH}:${VELOCITY_SHARED_LIB_ROOT}/${MACHINE}/${LIB_MODE}/${BUILD_MODE}/1/libvelocity.so" > "libvelocity_2.so"
+git show "${GIT_SOURCE_BRANCH}:${VELOCITY_SHARED_LIB_ROOT}/${MACHINE}/${LIB_MODE}/${BUILD_MODE}/1/libvelocity.so" > "libvelocity_3.so"
