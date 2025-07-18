8b2c1a22b1b39dce120be211e8b59455dc620c88
Pratyai Mazumder <pmazumde@ault.cscs.ch>
2025-07-18 17:18:37 +0200

    Update build script + precompiled libs.

--- velocity/build_libs.sh
+++ velocity/build_libs.sh
@@ -9,29 +9,26 @@ if [[ "$1" == "--clean" ]]; then
 fi
 
 RELEASE_MODES=(FALSE TRUE)
-INTEGRATION_MODES=(0 1)
 STAGES=(1 8)
 
 HOST=$(hostname)
 
 for _R in "${RELEASE_MODES[@]}"; do
-    for _BL in "${INTEGRATION_MODES[@]}"; do
         export _USE_CUDA_EVENTS=0
         export _TILE=0
         export _REDUCE_BITWIDTH_TRANSFORMATION=1
         export _RELEASE=${_R}
-        export _BUILD_LIB_FOR_SOLVE_NH=${_BL}
+        export _BUILD_LIB_FOR_SOLVE_NH=1
         for STAGE in "${STAGES[@]}"; do
             MODE=$( [[ "$_R" == "FALSE" ]] && echo "debug" || echo "release" )
-            LIBNAME="libvelocity_gpu_stage${STAGE}_standalone_${MODE}.so"
-            TARGET_DIR="./shared_libs/${HOST}/standalone/${MODE}/${STAGE}"
+            LIBNAME="libvelocity_gpu_stage${STAGE}_solve_nh_integration_${MODE}.so"
+            TARGET_DIR="./shared_libs/${HOST}/solve_nh_integration/${MODE}/${STAGE}"
             FINAL_LIB="${TARGET_DIR}/libvelocity.so"
 
             if [[ -f "$FINAL_LIB" ]]; then
                 echo "⚠️  $FINAL_LIB already exists — skipping compilation and move"
                 continue
             fi
-
             if [[ -f "$LIBNAME" ]]; then
                 echo "⚠️  $LIBNAME already exists — skipping compilation"
             else
@@ -48,5 +45,4 @@ for _R in "${RELEASE_MODES[@]}"; do
                 exit 1
             fi
         done
-    done
 done
