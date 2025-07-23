bfbad91d3b9634fb50b36e3e1d4de3d06cac6845
Yakup Budanaz <budanaz.yakup@gmail.com>
2025-07-23 17:38:32 +0200

    Stuff

--- velocity/build.sh
+++ velocity/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-RELEASE_MODES=(FALSE TRUE)
+RELEASE_MODES=(FALSE)
 INTEGRATION_MODES=(1 )
 
 for _R in "${RELEASE_MODES[@]}"; do
@@ -9,8 +9,6 @@ for _R in "${RELEASE_MODES[@]}"; do
         export _TILE=0
         export _REDUCE_BITWIDTH_TRANSFORMATION=1
         export _BUILD_LIB_FOR_SOLVE_NH=${BL}
-        python -m utils.stages.compile_gpu_stage1 --compile
-        python -m utils.stages.compile_gpu_stage8 --compile
         python -m utils.stages.compile_gpu_stage9 --compile
     done
 done
