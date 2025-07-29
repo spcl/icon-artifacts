395e4448a564f12f5d8577bc42a7b6e53096acc0
Yakup Budanaz <budanaz.yakup@gmail.com>
2025-07-29 12:55:27 +0200

    Fix stuff

--- solve_nh/utils/stage0_hack_split_predictor_pre_omp_loop.py
+++ solve_nh/utils/stage0_hack_split_predictor_pre_omp_loop.py
@@ -15,10 +15,20 @@ def split_predictor_pre_omp_loop(g: SDFG) -> None:
     )
     outerloop_succ = singular(ed for ed in box.out_edges(outerloop)).dst
 
-    outerloop_pt1 = outerloop
+    outerloop_pt1: LoopRegion = outerloop
     outerloop_pt1.label = f"{outerloop.label}_pt1"
-    outerloop_pt2 = deepcopy(outerloop)
+    outerloop_pt2: LoopRegion = deepcopy(outerloop)
     outerloop_pt2.label = f"{outerloop.label}_pt2"
+
+    for n in outerloop_pt2.all_control_flow_regions(recursive=True):
+        n.label = f"{n.label}_pt2"
+    for n in outerloop_pt1.all_control_flow_regions(recursive=True):
+        n.label = f"{n.label}_pt1"
+    for n in outerloop_pt1.all_states():
+        n.label = f"{n.label}_pt1"
+    for n in outerloop_pt2.all_states():
+        n.label = f"{n.label}_pt2"
+
     box.add_node(outerloop_pt2)
     for ed in box.out_edges(outerloop):
         box.remove_edge(ed)
@@ -26,9 +36,14 @@ def split_predictor_pre_omp_loop(g: SDFG) -> None:
     box.add_edge(outerloop_pt2, outerloop_succ, InterstateEdge())
 
     innerloop_pt1 = singular(n for n in outerloop_pt1.nodes())
-    for n in innerloop_pt1.nodes()[PT1_STATES:]:
+    for n in list(innerloop_pt1.bfs_nodes(innerloop_pt1.start_block))[PT1_STATES:]:
         innerloop_pt1.remove_node(n)
 
-    innerloop_pt2 = singular(n for n in outerloop_pt2.nodes())
-    for n in innerloop_pt2.nodes()[:PT1_STATES]:
+    innerloop_pt2: LoopRegion = singular(n for n in outerloop_pt2.nodes())
+    start_state = list(innerloop_pt2.bfs_nodes(innerloop_pt2.start_block))[PT1_STATES]
+    for n in list(innerloop_pt2.bfs_nodes(innerloop_pt2.start_block))[:PT1_STATES]:
         innerloop_pt2.remove_node(n)
+    assert start_state is not None, "No start state found in the inner loop pt2"
+    innerloop_pt2.start_block = innerloop_pt2.node_id(start_state)
+
+    g.validate()
